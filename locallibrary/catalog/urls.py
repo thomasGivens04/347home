054c7392a2b43280c2b2f3958b5ddd24162853ad
thomasGivens04 <givensta@dukes.jmu.edu>
2026-02-12 12:19:13 -0500

    Prep 4 addon

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -31,5 +33,38 @@ class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+    def get_queryset(self):
+        return Book.objects.all()
+    
+    template_name = 'catalog/book_list.html'
+
 class BookDetailView(generic.DetailView):
     model = Book
+
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+    
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'authors'
+
+    def get_queryset(self):
+        return Author.objects.all()
+    
+    template_name = 'catalog/author_list.html'
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def author_detail_view(request, primary_key):
+        book = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': book})
